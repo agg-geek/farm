194b3deed036bf7b535904ac6d9b12be5a520442
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-13 18:27:54 +0530

    Separate fns into modules

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+const fillTemplate = require('./modules/fillTemplate');
+
 const dataJSON = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
 const data = JSON.parse(dataJSON);
 
@@ -9,25 +11,7 @@ const templateOverview = fs.readFileSync(`${__dirname}/templates/overview.html`,
 const templateProductCard = fs.readFileSync(`${__dirname}/templates/product-card.html`, 'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 
-const fillTemplate = function (template, product) {
-	let output = template;
-	output = output.replace(/{%ID%}/g, product.id);
-	output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%NOT_ORGANIC%}/g, !product.organic ? 'not-organic' : '');
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-	return output;
-};
-
 const server = http.createServer((req, res) => {
-	// console.log(req.url);
-	// console.log(url.parse(req.url, true)); // true to parse the query string
-
 	const { query, pathname } = url.parse(req.url, true);
 
 	if (pathname === '/' || pathname === '/overview') {
