07c2a0830eb204d6827e8894118943aa0e0dc7f7
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-13 17:42:52 +0530

    Build templates

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const { type } = require('os');
 
-// the dot in ./ refers to the directory from where node command is ran
-// and not the directory in which index.js is present
-// this is not true for require()
-// console.log(__dirname);
 const dataJSON = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
 
 const data = JSON.parse(dataJSON);
@@ -20,7 +16,7 @@ const server = http.createServer((req, res) => {
 		res.end('This is PRODUCT');
 	} else if (pathName === '/api') {
 		res.writeHead(200, { 'Content-type': 'application/json' });
-		res.end(dataJSON); // res.end requires a string, so send the JSON string
+		res.end(dataJSON);
 	} else {
 		res.writeHead(404, { 'Content-type': 'text/html' });
 		res.end('<h1>Page not found</h1>');
